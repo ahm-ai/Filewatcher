2abe6e2cc6131f82e37e786d060b1b65ed67e098
ah <ah@example.com>
2024-02-08 09:01:56 -0600

    M

--- main.go
+++ main.go
@@ -15,8 +15,10 @@ import (
 	"github.com/fsnotify/fsnotify"
 )
 
-var currentCmd *exec.Cmd
-var commandMutex sync.Mutex
+var (
+	currentCmd   *exec.Cmd
+	commandMutex sync.Mutex
+)
 
 func expandPath(path string) (string, error) {
 	if strings.HasPrefix(path, "~") {
@@ -29,7 +31,36 @@ func expandPath(path string) (string, error) {
 	return path, nil
 }
 
+func killProcessOnPort(port string) {
+
+	if port == "" {
+		log.Printf("Port is empty")
+		return
+	}
+	// Find process using the port
+	cmd := exec.Command("lsof", "-t", "-i:"+port)
+	out, err := cmd.Output()
+	if err != nil {
+		log.Printf("Error finding process for port %s: %v", port, err)
+		return
+	}
+	pid := strings.TrimSpace(string(out))
+	if pid == "" {
+		log.Printf("No process found listening on port %s", port)
+		return
+	}
+	// Kill the process
+	killCmd := exec.Command("kill", pid)
+	err = killCmd.Run()
+	if err != nil {
+		log.Printf("Error killing process %s on port %s: %v", pid, port, err)
+	} else {
+		log.Printf("Successfully killed process %s on port %s", pid, port)
+	}
+}
+
 func main() {
+	appPort := flag.String("port", "3000", "Port to kill the process on")
 	pathFlag := flag.String("path", "", "Path to the folder to watch")
 	regexFlag := flag.String("regex", "", "Regex pattern to match file names")
 	commandFlag := flag.String("command", "", "Command to run on file change")
@@ -60,6 +91,7 @@ func main() {
 			if fileInfo.Name() == "node_modules" {
 				return filepath.SkipDir
 			}
+			fmt.Printf("\033[32mWatching directory: %s\033[0m\n", path)
 			return watcher.Add(path)
 		}
 		return nil
@@ -83,6 +115,8 @@ func main() {
 	executeCommand := func() {
 		commandMutex.Lock()
 		defer commandMutex.Unlock()
+
+		killProcessOnPort(*appPort)
 
 		// Kill the previous command if it's still running
 		if currentCmd != nil && currentCmd.Process != nil {
@@ -116,7 +150,7 @@ func main() {
 		go func() {
 			scanner := bufio.NewScanner(stdoutPipe)
 			for scanner.Scan() {
-				fmt.Printf("STDOUT: %s\n", scanner.Text())
+				fmt.Printf("%s\n", scanner.Text())
 			}
 		}()
 
@@ -124,7 +158,7 @@ func main() {
 		go func() {
 			scanner := bufio.NewScanner(stderrPipe)
 			for scanner.Scan() {
-				fmt.Printf("STDERR: %s\n", scanner.Text())
+				fmt.Printf("%s\n", scanner.Text())
 			}
 		}()
 
--- readme.md
+++ readme.md
@@ -30,3 +30,25 @@
    COMMAND_NAME="echo 'hi'"
     go run main.go --path="../" --regex=".*\\.js$" --command=$COMMAND_NAME
  ```
+
+### Use as a shell function anywhere.
+
+```sh
+
+shellFunction() {
+  PORT="8080"
+  WATCH_PATH="~/Documents/<PATH>"
+  WATCH_REGEX=".*\\.go$"
+  COMMAND_NAME=$(
+    cat <<EOF
+
+    <ANOTHER_SHELL_FUNCTION>
+
+EOF
+)
+
+  cd ~"/Documents/Filewatcher" 
+  go run main.go --path="$WATCH_PATH" --regex="$WATCH_REGEX" --command="$COMMAND_NAME" --port="$PORT"
+}
+
+```
